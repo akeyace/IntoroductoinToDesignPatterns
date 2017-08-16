16416389eb3caa4f8cde4ed04c2668961db4b19a
Ak Fu <a.fujii@akline.jp>
2017-08-16 23:17:48 +0900

    第三回 ソースコード完成版 #1

--- lessons/lesson_03/java/src/introductiontodesignpatterns/employee/Nagata.java
+++ lessons/lesson_03/java/src/introductiontodesignpatterns/employee/Nagata.java
@@ -1,6 +1,10 @@
 package introductiontodesignpatterns.employee;
 
 import introductiontodesignpatterns.answer.AnswerProxy;
+import introductiontodesignpatterns.workinformation.ConcreteWorkInformation;
+import introductiontodesignpatterns.workinformation.WorkInformationNagata;
+import introductiontodesignpatterns.workinformation.WorkInformationWithMemo;
+import introductiontodesignpatterns.workinformation.WorkInformationWithPrice;
 
 public class Nagata extends PEEmployee {
 	final private static Nagata self = new Nagata();
@@ -12,5 +16,12 @@ public class Nagata extends PEEmployee {
 
 	public static Nagata call() {
 		return self;
+	}
+
+	public void setWorkInformation(Number id, String workName, String detail, String memo, Number price) {
+		workInformations.put(id,
+				new WorkInformationNagata(new WorkInformationWithPrice(
+						new WorkInformationWithMemo(new ConcreteWorkInformation(workName, detail), memo), price))
+								.priceUp(1.2));
 	}
 }
